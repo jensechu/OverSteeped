a85aeb24a83df91063affa87a4988dbdfd70bf20
Jensen Hussey <jensechu@gmail.com>
2014-02-08 21:15:05 -0800

    :tea: Simple layouts

--- app.rb
+++ app.rb
@@ -10,7 +10,11 @@ class Tea < ActiveRecord::Base
 end
 
 get "/" do
-  @posts = Post.order("created_at DESC")
-  @title = "Welcome."
-  erb :"posts/index"
+  numCategories = Category.all.length
+  @categories = Category.take(numCategories)
+
+  numTeas = Tea.all.length
+  @teas = Tea.find([1, numTeas])
+
+  erb :"teas/index"
 end
